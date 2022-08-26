9e46493f52135c0210f3b87289601aa7750c9689
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2022-08-26 10:20:43 +0530

    feat: Did minor changes

---
+++ restdevelopment/account/commons/message.py
@@ -0,0 +1,4 @@
+USER_CREATED_SUCCESS='User created successfully.'
+USER_FOUND_SUCCESS='User record found successfully.'
+USER_UPDATED_SUCCESS='User updated successfully.'
+USER_DELETED_SUCCESS='User deleted successfully'
--- restdevelopment/account/views.py
+++ restdevelopment/account/views.py
@@ -5,7 +5,6 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import generics
 
 
 
